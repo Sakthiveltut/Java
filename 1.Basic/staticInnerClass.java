f7d54e2775a0bd32ee36640a7ddc20cd778d977e
sakthivetuty <msakthiveltut@gmail.com>
2024-02-11 18:05:04 +0530

    static

---
+++ 1.Basic/staticInnerClass.java
@@ -0,0 +1,18 @@
+class OuterClass {
+    static int x=10;
+    int y=20;
+    static class InnerClass
+    {
+        void display()
+        {
+            System.out.println("X : "+x);
+        }
+    }
+}
+ 
+public class staticInnerClass {
+    public static void main(String[] args) {
+        OuterClass.InnerClass i =new OuterClass.InnerClass();
+        i.display();
+    }
+}
